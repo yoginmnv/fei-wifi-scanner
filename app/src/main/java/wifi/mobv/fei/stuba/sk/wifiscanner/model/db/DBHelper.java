f0b116e991e9181c44fcdec65b0ea4e93881902b
YogiNMnV <yoginmnv@gmail.com>
2016-12-03 21:37:05 +0100

    cosmetic update

--- app/src/main/java/wifi/mobv/fei/stuba/sk/wifiscanner/model/db/DBHelper.java
+++ app/src/main/java/wifi/mobv/fei/stuba/sk/wifiscanner/model/db/DBHelper.java
@@ -17,19 +17,19 @@ import wifi.mobv.fei.stuba.sk.wifiscanner.model.db.dao.HistoryDAO.HistoryEntry;
 public class DBHelper extends SQLiteOpenHelper
 {
 	public static final String TAG = DBHelper.class.getSimpleName();
-	private static DBHelper instance;
-
 	// If you change the database schema, you must increment the database version.
-	public static final int DATABASE_VERSION = 3;
 	public static final String DATABASE_NAME = "wifi.db";
+	public static final int DATABASE_VERSION = 3;
 
-	public static final String SQL_CREATE_LOCATION_ENTRY = "CREATE TABLE IF NOT EXISTS " + LocationEntry.TABLE_NAME + " (" +
+	public static final String SQL_CREATE_LOCATION_ENTRY =
+			"CREATE TABLE IF NOT EXISTS " + LocationEntry.TABLE_NAME + " (" +
 			LocationEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 			LocationEntry.COLUMN_NAME_BLOCK_NAME + " TEXT, " +
 			LocationEntry.COLUMN_NAME_FLOOR + " TEXT, " +
 			"UNIQUE(" + LocationEntry.COLUMN_NAME_BLOCK_NAME + ", " + LocationEntry.COLUMN_NAME_FLOOR + ") ON CONFLICT IGNORE);";
 
-	public static final String SQL_CREATE_WIFI_ENTRY = "CREATE TABLE IF NOT EXISTS " + WifiEntry.TABLE_NAME + " (" +
+	public static final String SQL_CREATE_WIFI_ENTRY =
+			"CREATE TABLE IF NOT EXISTS " + WifiEntry.TABLE_NAME + " (" +
 			WifiEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 			WifiEntry.COLUMN_NAME_ID_LOCATION + " INTEGER NOT NULL, " +
 			WifiEntry.COLUMN_NAME_BSSID + " TEXT NOT NULL, " +
@@ -38,11 +38,14 @@ public class DBHelper extends SQLiteOpenHelper
 			"FOREIGN KEY(" + WifiEntry.COLUMN_NAME_ID_LOCATION + ") REFERENCES " + LocationEntry.TABLE_NAME + "(" + LocationEntry._ID + "), " +
 			"UNIQUE(" + WifiEntry.COLUMN_NAME_BSSID + ") ON CONFLICT IGNORE);";
 
-	public static final String SQL_CREATE_HISTORY_ENTRY = "CREATE TABLE IF NOT EXISTS " + HistoryEntry.TABLE_NAME + " (" +
+	public static final String SQL_CREATE_HISTORY_ENTRY =
+			"CREATE TABLE IF NOT EXISTS " + HistoryEntry.TABLE_NAME + " (" +
 			HistoryEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
 			HistoryEntry.COLUMN_NAME_ID_LOCATION + " INTEGER NOT NULL, " +
 			HistoryEntry.COLUMN_NAME_DATE + " TEXT NOT NULL, " +
 			"FOREIGN KEY(" + HistoryEntry.COLUMN_NAME_ID_LOCATION + ") REFERENCES " + LocationEntry.TABLE_NAME + "(" + LocationEntry._ID + "));";
+
+	private static DBHelper instance;
 
 	private DBHelper(Context context)
 	{
